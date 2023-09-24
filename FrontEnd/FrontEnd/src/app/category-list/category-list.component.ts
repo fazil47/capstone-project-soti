6227ef5f6730f125c56eba97f16c195cf1ee28c1
sinad-shibin <sinadinshibin2@gmail.com>
2023-09-24 19:15:40 +0530

    category list table fist commit

--- FrontEnd/FrontEnd/src/app/category-list/category-list.component.ts
+++ FrontEnd/FrontEnd/src/app/category-list/category-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryListService } from './categorylist.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit{
+constructor(public serv : CategoryListService){}
 
+ngOnInit(): void {
+    this.serv.refreshCategoryList();
+}
 }
