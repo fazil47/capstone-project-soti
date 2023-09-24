27942110d1a8af8100f083be71a8aeeb6aa8bf85
sinad-shibin <sinadinshibin2@gmail.com>
2023-09-24 19:48:59 +0530

    centralised UI

--- FrontEnd/FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/FrontEnd/src/app/login/login.component.ts
@@ -1,37 +1,68 @@
-import { Component,OnInit } from '@angular/core';
-import { Login } from '../shared/models/login.model';
-import { NgForm } from '@angular/forms';
-import { LoginService } from '../shared/services/login.service';
+// import { Component, OnInit } from '@angular/core';
+// import { Login } from '../shared/models/login.model';
+// import { NgForm } from '@angular/forms';
+// import { LoginService } from '../shared/services/login.service';
 
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css'],
+// })
+// export class LoginComponent implements OnInit {
+//   credentials: Login = { emailid: '', password: '' };
+
+//   constructor(public logServ: LoginService) {}
+
+//   ngOnInit(): void {
+//     if (localStorage.getItem('currentUser') != null) {
+//       this.logServ.name = JSON.parse(localStorage.getItem('currentUser')).name;
+//     }
+//   }
+
+//   login(form: NgForm) {
+//     if (form.valid) {
+//       this.logServ.login(form, this.credentials);
+//     }
+//   }
+
+//   logOut = () => {
+//     this.logServ.logout();
+//   };
+// }
+
+import { Component, OnInit } from '@angular/core';
+
+import { Login } from '../shared/models/login.model';
+
+import { NgForm } from '@angular/forms';
+
+import { LoginService } from '../shared/services/login.service';
 
 @Component({
   selector: 'app-login',
+
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-  credentials: Login = {emailid:'', password:''};
-  
-  constructor(public logServ:LoginService){}
+export class LoginComponent implements OnInit {
+  credentials: Login = { emailid: '', password: '' };
+
+  constructor(public logServ: LoginService) {}
 
   ngOnInit(): void {
-    if(localStorage.getItem("currentUser")!=null)
-    {
-    this.logServ.name = JSON.parse(localStorage.getItem("currentUser")).name;
+    if (localStorage.getItem('currentUser') != null) {
+      this.logServ.name = JSON.parse(localStorage.getItem('currentUser')).name;
     }
   }
 
-  login(form:NgForm)
-  {
+  login(form: NgForm) {
     if (form.valid) {
-      this.logServ.login(form,this.credentials);
+      this.logServ.login(form, this.credentials);
     }
   }
-  
 
-  
   logOut = () => {
     this.logServ.logout();
-  }
-
+  };
 }
