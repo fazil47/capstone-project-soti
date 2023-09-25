964925fc7e96261c51960002f7fbd6ec5ce5453c
fazil <fazil47babu@gmail.com>
2023-09-25 14:43:44 +0530

    Added product details page

--- FrontEnd/FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/FrontEnd/src/app/app-routing.module.ts
@@ -9,13 +9,19 @@ import { DetailsComponent } from './details/details.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './guards/auth.guard';
 import { RegisterComponent } from './register/register.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
   { path: 'contact', component: ContactComponent, canActivate: [AuthGuard] },
   { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
-  { path: 'details', component: DetailsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'products/:productId',
+    component: ProductDetailsComponent,
+    canActivate: [AuthGuard],
+  },
+  // { path: 'details', component: DetailsComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
